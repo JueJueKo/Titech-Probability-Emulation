272c835cd622152cd65b1f58b2e01bedcc3493c0
JueJueKo <90765938+JueJueKo@users.noreply.github.com>
2021-10-08 23:42:40 +0900

    Add files via upload
    added the code moded for plotting

---
+++ sim_3rd/sim_3rd.m
@@ -0,0 +1,24 @@
+clear;clc;
+
+ropenum = 100;
+head = [1:ropenum].';
+round = 100000;
+count = 0;
+
+for j = 1:round
+    tail = randperm(ropenum).';
+    state = [head,tail];
+    state = randomize(state);
+    ite = 0;
+    sum1 = sum(state(:,1));
+    while ite < ropenum
+        ite=ite+1;
+        if state(ite,1)~=0
+            state= findloop(state,ite);
+            count = count + 1;
+        end
+    end
+end
+
+expta = count / round
+
